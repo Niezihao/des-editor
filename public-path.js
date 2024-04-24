b27e3c05155e0d8b6139d1bf748679a68c5d34b3
CHINAMI-MC6VNQQ\EDY <1332421989@qq.com>
2024-04-24 18:56:46 +0800

    feat: init

---
+++ public-path.js
@@ -0,0 +1,12 @@
+/*
+ * @Author: CHINAMI-MC6VNQQ\EDY 1332421989@qq.com
+ * @Date: 2024-04-24 18:31:22
+ * @LastEditors: CHINAMI-MC6VNQQ\EDY 1332421989@qq.com
+ * @LastEditTime: 2024-04-24 18:31:32
+ * @FilePath: \mall-cook-platform\public-path.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+
+if (window.__POWERED_BY_QIANKUN__) {
+    __webpack_public_path__ = window.__INJECTED_PUBLIC_PATH_BY_QIANKUN__;
+}
